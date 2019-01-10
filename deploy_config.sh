0ef0349e612de3a7e0d32c7723074e3ac36c635a
Andrew Evans <andrew.evans@rhythmandbinary.com>
2019-01-10 10:48:48 -0500

    deploy again

--- deploy_config.sh
+++ deploy_config.sh
@@ -1,15 +1,14 @@
-export API_KEY='a'
-export AUTH_DOMAIN='b'
-export DATABASE_URL='c'
-export PROJECT_ID='d'
-export STORAGE_BUCKET='e'
-export MESSAGE_SENDER_ID='f'
-
+# !/bin/bash
+# deploy_config.sh
+# This script updates Firebase Environment Variables at deployment
 # Note that sed on OSX and sed in Linux operate different ways
 # with OSX you have to pass a dummy blank file with -i like 'sed -i "" <s command> <file>'
+
+cd src/environments
 sed -i 's/\(API_KEY\)/'$API_KEY'/' environment.prod.ts
 sed -i 's/\(AUTH_DOMAIN\)/'$AUTH_DOMAIN'/' environment.prod.ts
 sed -i 's/\(DATABASE_URL\)/'$DATABASE_URL'/' environment.prod.ts
 sed -i 's/\(PROJECT_ID\)/'$PROJECT_ID'/' environment.prod.ts
 sed -i 's/\(STORAGE_BUCKET\)/'$STORAGE_BUCKET'/' environment.prod.ts
 sed -i 's/\(MESSAGE_SENDER_ID\)/'$MESSAGING_SENDER_ID'/' environment.prod.ts
+cat environment.prod.ts
