dc27a4f3584c7df5834e807d77f38d6183d70af8
Andrew Evans <andrew.evans@rhythmandbinary.com>
2019-01-10 10:24:04 -0500

    fixing locator for file in sed command

--- deploy_config.sh
+++ deploy_config.sh
@@ -9,14 +9,13 @@ echo 'where are we here'
 pwd
 ls
 
-
-sed -i '' 's/\(API_KEY\)/'$API_KEY'/' /src/environments/environment.ts
-sed -i '' 's/\(AUTH_DOMAIN\)/'$AUTH_DOMAIN'/' /src/environments/environment.ts
-sed -i '' 's/\(DATABASE_URL\)/'$DATABASE_URL'/' /src/environments/environment.ts
-sed -i '' 's/\(PROJECT_ID\)/'$PROJECT_ID'/' /src/environments/environment.ts
-sed -i '' 's/\(STORAGE_BUCKET\)/'$STORAGE_BUCKET'/' /src/environments/environment.ts
-sed -i '' 's/\(MESSAGE_SENDER_ID\)/'$MESSAGE_SENDER_ID'/' /src/environments/environment.ts
+sed -i '' 's/\(API_KEY\)/'$API_KEY'/' src/environments/environment.ts
+sed -i '' 's/\(AUTH_DOMAIN\)/'$AUTH_DOMAIN'/' src/environments/environment.ts
+sed -i '' 's/\(DATABASE_URL\)/'$DATABASE_URL'/' src/environments/environment.ts
+sed -i '' 's/\(PROJECT_ID\)/'$PROJECT_ID'/' src/environments/environment.ts
+sed -i '' 's/\(STORAGE_BUCKET\)/'$STORAGE_BUCKET'/' src/environments/environment.ts
+sed -i '' 's/\(MESSAGE_SENDER_ID\)/'$MESSAGE_SENDER_ID'/' src/environments/environment.ts
 
 # rm -rf 'environment.ts.backup'
 
-cat /src/environments/environment.ts
+cat src/environments/environment.ts
