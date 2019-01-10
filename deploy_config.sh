2e4ab55af6191e9f740a8ac478205488c136a444
Andrew Evans <andrew.evans@rhythmandbinary.com>
2019-01-10 10:32:33 -0500

    debug

--- deploy_config.sh
+++ deploy_config.sh
@@ -10,12 +10,12 @@ pwd
 ls
 cd src/environments
 
-sed -i '' 's/\(API_KEY\)/'$API_KEY'/' environment.ts
-sed -i '' 's/\(AUTH_DOMAIN\)/'$AUTH_DOMAIN'/' environment.ts
-sed -i '' 's/\(DATABASE_URL\)/'$DATABASE_URL'/' environment.ts
-sed -i '' 's/\(PROJECT_ID\)/'$PROJECT_ID'/' environment.ts
-sed -i '' 's/\(STORAGE_BUCKET\)/'$STORAGE_BUCKET'/' environment.ts
-sed -i '' 's/\(MESSAGE_SENDER_ID\)/'$MESSAGE_SENDER_ID'/' environment.ts
+sed -i 's/\(API_KEY\)/'$API_KEY'/' environment.ts
+sed -i 's/\(AUTH_DOMAIN\)/'$AUTH_DOMAIN'/' environment.ts
+sed -i 's/\(DATABASE_URL\)/'$DATABASE_URL'/' environment.ts
+sed -i 's/\(PROJECT_ID\)/'$PROJECT_ID'/' environment.ts
+sed -i 's/\(STORAGE_BUCKET\)/'$STORAGE_BUCKET'/' environment.ts
+sed -i 's/\(MESSAGE_SENDER_ID\)/'$MESSAGE_SENDER_ID'/' environment.ts
 
 # rm -rf 'environment.ts.backup'
 
