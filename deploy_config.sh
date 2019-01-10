d073290295174a9a2368bc2f0a107670a9876355
Andrew Evans <andrew.evans@rhythmandbinary.com>
2019-01-10 10:26:13 -0500

    debug

--- deploy_config.sh
+++ deploy_config.sh
@@ -8,14 +8,15 @@ export MESSAGE_SENDER_ID='f'
 echo 'where are we here'
 pwd
 ls
+cd src/environments
 
-sed -i '' 's/\(API_KEY\)/'$API_KEY'/' src/environments/environment.ts
-sed -i '' 's/\(AUTH_DOMAIN\)/'$AUTH_DOMAIN'/' src/environments/environment.ts
-sed -i '' 's/\(DATABASE_URL\)/'$DATABASE_URL'/' src/environments/environment.ts
-sed -i '' 's/\(PROJECT_ID\)/'$PROJECT_ID'/' src/environments/environment.ts
-sed -i '' 's/\(STORAGE_BUCKET\)/'$STORAGE_BUCKET'/' src/environments/environment.ts
-sed -i '' 's/\(MESSAGE_SENDER_ID\)/'$MESSAGE_SENDER_ID'/' src/environments/environment.ts
+sed -i '' 's/\(API_KEY\)/'$API_KEY'/' environment.ts
+sed -i '' 's/\(AUTH_DOMAIN\)/'$AUTH_DOMAIN'/' environment.ts
+sed -i '' 's/\(DATABASE_URL\)/'$DATABASE_URL'/' environment.ts
+sed -i '' 's/\(PROJECT_ID\)/'$PROJECT_ID'/' environment.ts
+sed -i '' 's/\(STORAGE_BUCKET\)/'$STORAGE_BUCKET'/' environment.ts
+sed -i '' 's/\(MESSAGE_SENDER_ID\)/'$MESSAGE_SENDER_ID'/' environment.ts
 
 # rm -rf 'environment.ts.backup'
 
-cat src/environments/environment.ts
+cat environment.ts
