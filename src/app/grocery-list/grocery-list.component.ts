e9efb39dd733e08a088273ec84651e33e03ff8cc
Andrew Evans <andrew.evans@rhythmandbinary.com>
2019-01-26 08:33:41 -0500

    cleaning up app and syntax

--- src/app/grocery-list/grocery-list.component.ts
+++ src/app/grocery-list/grocery-list.component.ts
@@ -15,11 +15,8 @@ export class GroceryListComponent {
   createItem = '';
   showLoginUserInputForm = false;
   showCreateUserInputForm = false;
-  loginEmail = '';
-  loginPassword = '';
-  createEmail = '';
-  createPassword = '';
-  userEmail = '';
+  email = '';
+  password = '';
   // create doc of type Item that represents the individual GroceryItems nested collection
   groceryItemsDoc: AngularFirestoreDocument<Item>;
   groceryItems: Observable<GroceryItem[]>;
@@ -27,8 +24,8 @@ export class GroceryListComponent {
   constructor(public afs: AngularFirestore, public afAuth: AngularFireAuth) {
     this.afAuth.auth.onAuthStateChanged(user => {
       if (user) {
-        // when logged in show user email here
-        this.userEmail = user.email;
+        // show email in welcome message
+        this.email = user.email;
         // call method that selects all items when user is authenticated
         this.selectItems(user.uid);
       }
@@ -43,22 +40,20 @@ export class GroceryListComponent {
       id: id
     };
 
-    this.groceryItemsDoc.collection<GroceryItem>('GroceryItems').doc(id).set(groceryItem)
-      .then(
-        function(success) {
+    await this.groceryItemsDoc.collection<GroceryItem>('GroceryItems').doc(id).set(groceryItem)
+      .then(() => {
           // when successful clear input field value here
           this.createItem = '';
-        }.bind(this),
-        function(error) {
-          alert(error);
-        }
-      );
+      })
+      .catch((error) => {
+        alert(error);
+      });
   }
 
   // async is not necessary here, but using it to control event loop
   async deleteItem(groceryItem: GroceryItem) {
-    this.groceryItemsDoc.collection<GroceryItem>('GroceryItems').doc(groceryItem.id).delete()
-      .catch(function(error) { alert(error); });
+    await this.groceryItemsDoc.collection<GroceryItem>('GroceryItems').doc(groceryItem.id).delete()
+      .catch((error) => { alert(error); });
   }
 
   showLoginUserForm() {
@@ -69,58 +64,51 @@ export class GroceryListComponent {
     this.showCreateUserInputForm = true;
   }
 
-  createUser() {
-    this.afAuth.auth.createUserWithEmailAndPassword(this.createEmail, this.createPassword)
-      .then(
-        function(success) {
+  createUser(email: string, password: string) {
+    this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then(() => {
           // on success hide create user input form and store variables in login
           // and then call the login method
           this.showCreateUserInputForm = false;
-          this.loginEmail = this.createEmail;
-          this.loginPassword = this.createPassword;
-          this.createEmail = '';
-          this.createPassword = '';
-          this.loginUser();
-        }.bind(this),
-        function(error) {
-          alert(error);
+          this.loginUser(email, password);
+      })
+      .catch((error) => {
+        alert(error);
       });
   }
 
-  loginUser() {
-    this.afAuth.auth.signInWithEmailAndPassword(this.loginEmail, this.loginPassword)
-      .then(
-        function(success) {
-          // on success populate user variables and then select grocery items for that user
-          this.userUiD = success.user.uid;
-          this.userEmail = success.user.email;
-          this.loginEmail = '';
-          this.loginPassword = '';
-          this.showLoginUserInputForm = false;
-          this.showCreateUserInputForm = false;
-          this.userEmail = this.afAuth.auth.currentUser.email;
-          this.selectItems(this.afAuth.auth.currentUser.uid);
-        }.bind(this),
-        function(error) {
-          alert(error);
+  loginUser(email: string, password: string) {
+    this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then(() => {
+        // on success populate user variables and then select grocery items for that user
+        this.selectItems(this.afAuth.auth.currentUser.uid);
+      })
+      .catch((error) => {
+        alert(error);
       });
   }
 
   selectItems(uid: string) {
     this.groceryItemsDoc = this.afs.doc<Item>('user/' + uid);
     this.groceryItems = this.groceryItemsDoc.collection<GroceryItem>('GroceryItems').valueChanges();
-    // turn on logging if you want to see how the requests are sent
-    this.groceryItemsDoc.collection<GroceryItem>('GroceryItems').auditTrail().subscribe(console.log);
+    // // turn on logging if you want to see how the requests are sent
+    // this.groceryItemsDoc.collection<GroceryItem>('GroceryItems').auditTrail().subscribe(console.log);
   }
 
   // async is not necessary here, just controlling the event loop
   async logoutUser() {
     await this.afAuth.auth.signOut()
       .catch(function(error) { alert(error); });
+
+    this.email = '';
+    this.password = '';
+    this.showLoginUserInputForm = false;
+    this.showCreateUserInputForm = false;
   }
 
   cancelButton() {
     this.showLoginUserInputForm = false;
     this.showCreateUserInputForm = false;
   }
+
 }
